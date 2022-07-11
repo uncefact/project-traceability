a31cbee6ec6c2836ae9726622b62f98162cec688
Kseniya Shychko <k.shychko@gmail.com>
2022-07-11 11:10:57 +0200

    feat: structure refactoring and pulumi initial config (#28)
    * feat: structure refactoring and pulumi initial config
    
    * feat: update folders naming to align with the approach one api per repo
    
    * feat: pack source api files under src folder
    
    * feat: add initial postman collection

---
+++ config/scripts/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#set -e
+
+pip install -U pip setuptools wheel
+pip install pdm
+if [ -d "tmp" ]; then
+  rm -rf "tmp"
+fi
+mkdir "tmp"
+cp -r ../mock/* "tmp"
+cd "tmp"
+pdm sync --prod --no-editable
+cd __pypackages__/3.9/lib
+zip -r ../../../deployment-package.zip .
+cd ../../../
+zip -g deployment-package.zip src/*.py
